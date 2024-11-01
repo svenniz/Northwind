390ae0c09e1a969f542f22d1740b5634220a24f5
Andreas Ventzel <a-spac01@specialisterne.dk>
2024-11-01 13:36:43 +0100

    added vs solution

---
+++ Northwind/Northwind/Data/NorthwindContext.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Models;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace Northwind.Data;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<Orderdetail> Orderdetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Northwind.Models.Region> Regions { get; set; }
+
+    public virtual DbSet<Shipper> Shippers { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    public virtual DbSet<Territory> Territories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=northwind;user=root;password=Password1!", ServerVersion.Parse("9.1.0-mysql"));
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PRIMARY");
+
+            entity.ToTable("categories");
+
+            entity.HasIndex(e => e.CategoryName, "CategoryName");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName).HasMaxLength(15);
+            entity.Property(e => e.Description).HasColumnType("mediumtext");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PRIMARY");
+
+            entity.ToTable("customers");
+
+            entity.HasIndex(e => e.City, "City");
+
+            entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.HasIndex(e => e.Region, "Region");
+
+            entity.Property(e => e.CustomerId)
+                .HasMaxLength(5)
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.ContactName).HasMaxLength(30);
+            entity.Property(e => e.ContactTitle).HasMaxLength(30);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Fax).HasMaxLength(24);
+            entity.Property(e => e.Phone).HasMaxLength(24);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PRIMARY");
+
+            entity.ToTable("employees");
+
+            entity.HasIndex(e => e.LastName, "LastName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.FirstName).HasMaxLength(10);
+            entity.Property(e => e.HireDate).HasColumnType("datetime");
+            entity.Property(e => e.LastName).HasMaxLength(20);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+            entity.Property(e => e.Title).HasMaxLength(30);
+            entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
+
+            entity.HasMany(d => d.Territories).WithMany(p => p.Employees)
+                .UsingEntity<Dictionary<string, object>>(
+                    "Employeeterritory",
+                    r => r.HasOne<Territory>().WithMany()
+                        .HasForeignKey("TerritoryId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_EmployeeTerritories_Territories"),
+                    l => l.HasOne<Employee>().WithMany()
+                        .HasForeignKey("EmployeeId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_EmployeeTerritories_Employees"),
+                    j =>
+                    {
+                        j.HasKey("EmployeeId", "TerritoryId")
+                            .HasName("PRIMARY")
+                            .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+                        j.ToTable("employeeterritories");
+                        j.HasIndex(new[] { "TerritoryId" }, "FK_EmployeeTerritories_Territories");
+                        j.IndexerProperty<int>("EmployeeId").HasColumnName("EmployeeID");
+                        j.IndexerProperty<string>("TerritoryId")
+                            .HasMaxLength(20)
+                            .HasColumnName("TerritoryID");
+                    });
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PRIMARY");
+
+            entity.ToTable("orders");
+
+            entity.HasIndex(e => e.CustomerId, "FK_Orders_Customers");
+
+            entity.HasIndex(e => e.EmployeeId, "FK_Orders_Employees");
+
+            entity.HasIndex(e => e.ShipVia, "FK_Orders_Shippers");
+
+            entity.HasIndex(e => e.OrderDate, "OrderDate");
+
+            entity.HasIndex(e => e.ShipPostalCode, "ShipPostalCode");
+
+            entity.HasIndex(e => e.ShippedDate, "ShippedDate");
+
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.CustomerId)
+                .HasMaxLength(5)
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Freight)
+                .HasPrecision(10, 4)
+                .HasDefaultValueSql("'0.0000'");
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.RequiredDate).HasColumnType("datetime");
+            entity.Property(e => e.ShipAddress).HasMaxLength(60);
+            entity.Property(e => e.ShipCity).HasMaxLength(15);
+            entity.Property(e => e.ShipCountry).HasMaxLength(15);
+            entity.Property(e => e.ShipName).HasMaxLength(40);
+            entity.Property(e => e.ShipPostalCode).HasMaxLength(10);
+            entity.Property(e => e.ShipRegion).HasMaxLength(15);
+            entity.Property(e => e.ShippedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK_Orders_Customers");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK_Orders_Employees");
+
+            entity.HasOne(d => d.ShipViaNavigation).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ShipVia)
+                .HasConstraintName("FK_Orders_Shippers");
+        });
+
+        modelBuilder.Entity<Orderdetail>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ProductId })
+                .HasName("PRIMARY")
+                .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0 });
+
+            entity.ToTable("orderdetails");
+
+            entity.HasIndex(e => e.ProductId, "FK_Order_Details_Products");
+
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Discount).HasColumnType("double(8,0)");
+            entity.Property(e => e.Quantity).HasDefaultValueSql("'1'");
+            entity.Property(e => e.UnitPrice).HasPrecision(10, 4);
+
+            entity.HasOne(d => d.Order).WithMany(p => p.Orderdetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Order_Details_Orders");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Orderdetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Order_Details_Products");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PRIMARY");
+
+            entity.ToTable("products");
+
+            entity.HasIndex(e => e.CategoryId, "FK_Products_Categories");
+
+            entity.HasIndex(e => e.SupplierId, "FK_Products_Suppliers");
+
+            entity.HasIndex(e => e.ProductName, "ProductName");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Discontinued)
+                .HasDefaultValueSql("b'0'")
+                .HasColumnType("bit(1)");
+            entity.Property(e => e.ProductName).HasMaxLength(40);
+            entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("'0'");
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.UnitPrice)
+                .HasPrecision(10, 4)
+                .HasDefaultValueSql("'0.0000'");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("'0'");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("'0'");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Products_Categories");
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.Products)
+                .HasForeignKey(d => d.SupplierId)
+                .HasConstraintName("FK_Products_Suppliers");
+        });
+
+        modelBuilder.Entity<Northwind.Models.Region>(entity =>
+        {
+            entity.HasKey(e => e.RegionId).HasName("PRIMARY");
+
+            entity.ToTable("region");
+
+            entity.Property(e => e.RegionId)
+                .ValueGeneratedNever()
+                .HasColumnName("RegionID");
+            entity.Property(e => e.RegionDescription).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Shipper>(entity =>
+        {
+            entity.HasKey(e => e.ShipperId).HasName("PRIMARY");
+
+            entity.ToTable("shippers");
+
+            entity.Property(e => e.ShipperId).HasColumnName("ShipperID");
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.Phone).HasMaxLength(24);
+        });
+
+        modelBuilder.Entity<Supplier>(entity =>
+        {
+            entity.HasKey(e => e.SupplierId).HasName("PRIMARY");
+
+            entity.ToTable("suppliers");
+
+            entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.ContactName).HasMaxLength(30);
+            entity.Property(e => e.ContactTitle).HasMaxLength(30);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Fax).HasMaxLength(24);
+            entity.Property(e => e.HomePage).HasColumnType("mediumtext");
+            entity.Property(e => e.Phone).HasMaxLength(24);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+        });
+
+        modelBuilder.Entity<Territory>(entity =>
+        {
+            entity.HasKey(e => e.TerritoryId).HasName("PRIMARY");
+
+            entity.ToTable("territories");
+
+            entity.HasIndex(e => e.RegionId, "FK_Territories_Region");
+
+            entity.Property(e => e.TerritoryId)
+                .HasMaxLength(20)
+                .HasColumnName("TerritoryID");
+            entity.Property(e => e.RegionId).HasColumnName("RegionID");
+            entity.Property(e => e.TerritoryDescription).HasMaxLength(50);
+
+            entity.HasOne(d => d.Region).WithMany(p => p.Territories)
+                .HasForeignKey(d => d.RegionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Territories_Region");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Northwind/Northwind/Program.cs
@@ -0,0 +1,165 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Northwind.Data;
+using Northwind.Models;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Windows.Forms;
+using System.Linq;
+using OxyPlot.Axes;
+
+//Application.EnableVisualStyles();
+//Application.SetCompatibleTextRenderingDefault(false);
+
+using (var context = new NorthwindContext())
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Enter which Northwind Query you want to see: ");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("ListEmployees: ");
+            ListEmployees(context);
+            break;
+        case "2":
+            Console.WriteLine("ListProductsDescPrice: ");
+            ListProductsDescPrice(context);
+            break;
+        case "3":
+            Console.WriteLine("ListCustomersFromSpainOrUk: ");
+            ListCustomersFromSpainOrUk(context);
+            break;
+        case "4":
+            Console.WriteLine("ListProductsWithStockOver100: ");
+            ListProductsWithStockOver100(context);
+            break;
+        case "5":
+            Console.WriteLine("ListShipCountriesWithOrderOnce: ");
+            ListShipCountriesWithOrderOnce(context);
+            break;
+        case "6":
+            Console.WriteLine("ListOrdersFromOctober1996: ");
+            ListOrdersFromOctober1996(context);
+            break;
+        case "7":
+            Console.WriteLine("ListTotalSalesFromEachDistinctCountry: ");
+            ListTotalSalesFromEachDistinctCountry(context);
+            break;
+        case "8":
+            Console.WriteLine("PlotCategories: ");
+            PlotCategories(context);
+            break;
+        default:
+            break;
+    }
+}
+static void ListEmployees(NorthwindContext context)
+{
+    var employees = context.Employees.ToList();
+    foreach (var employee in employees)
+    {
+        Console.WriteLine($"Employee: {employee.FirstName} {employee.LastName}");
+    }
+}
+static void ListProductsDescPrice(NorthwindContext context)
+{
+    var products = context.Products
+        .OrderByDescending(p=>p.UnitPrice)
+        .ToList();
+    foreach( var product in products)
+    {
+        Console.WriteLine($"Product {product.ProductName} has price: {product.UnitPrice}");
+    }
+}
+static void ListCustomersFromSpainOrUk(NorthwindContext context)
+{
+    var customers = context.Customers
+        .Where(c => c.Country.Equals("Spain") || c.Country.Equals("UK"))
+        .ToList();
+    foreach (var customer in customers)
+    {
+        Console.WriteLine($"Customer: {customer.ContactName} in Country: {customer.Country}");
+    }
+}
+static void ListProductsWithStockOver100(NorthwindContext context)
+{
+    var products = context.Products
+        .Where(p => p.UnitPrice >= 25 && p.UnitsInStock > 100)
+        .ToList();
+    foreach (var product in products)
+    {
+        Console.WriteLine($"Product: {product.ProductName} with Price: {product.UnitPrice} has Stock: {product.UnitsInStock}");
+    }
+}
+static void ListShipCountriesWithOrderOnce(NorthwindContext context)
+{
+    var shipCountries = context.Orders
+        .Select(o=>o.ShipCountry)
+        .Distinct()
+        .ToList();
+    foreach(var shipCountry in shipCountries)
+    {
+        Console.WriteLine($"Country: {shipCountry}");
+    }
+}
+static void ListOrdersFromOctober1996(NorthwindContext context)
+{
+    DateTime startDate = new DateTime(1996, 10, 1);
+    DateTime endDate = new DateTime(1996, 10, 31);
+
+    var orders = context.Orders
+        .Where(o=> o.OrderDate >= startDate && o.OrderDate <= endDate)
+        .ToList();
+    foreach (var order in orders)
+    {
+        Console.WriteLine($"{order.ShipName}");
+    }
+}
+static void ListTotalSalesFromEachDistinctCountry(NorthwindContext context)
+{
+    var countrySales = context.Orders
+        .Join(context.Orderdetails,
+        o=>o.OrderId, od=>od.OrderId,
+        (o,od)=> new {o.ShipCountry, od.UnitPrice, od.Quantity })
+        .GroupBy(x=>x.ShipCountry)
+        .Select(g=> new
+        {
+            ShipCountry = g.Key,
+            TotalSales = g.Sum(x=>x.UnitPrice*x.Quantity)
+        })
+        .OrderBy(x=>x.ShipCountry)
+        .ToList();
+    foreach (var country in countrySales)
+    {
+        Console.WriteLine($"{country.ShipCountry} {country.TotalSales}");
+    }
+}
+static void PlotCategories(NorthwindContext context)
+{
+    var categoryCounts = context.Categories
+        .Select(c=> new
+        {
+            CategoryName = c.CategoryName,
+            ProductCount = c.Products.Count(p=>p.CategoryId == c.CategoryId)
+        })
+        .ToList();
+
+    var model = new PlotModel { Title = "Products per Category" };
+    var barSeries = new BarSeries
+    {
+        Title = "Products"
+    };
+    foreach (var category in categoryCounts)
+    {
+        barSeries.Items.Add(new BarItem { Value = category.ProductCount });
+    }
+    model.Series.Add(barSeries);
+
+    var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
+    foreach (var category in categoryCounts)
+    {
+        categoryAxis.Labels.Add(category.CategoryName);
+    }
+    model.Axes.Add(categoryAxis);
+}
